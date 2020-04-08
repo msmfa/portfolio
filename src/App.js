4820d53863dc9ec4a000311d28ac0736560aa621
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-08 22:34:29 +0100

    Updated App formatting

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ class App extends Component {
         <ProjectHeader />
         <ProjectSection />
         <ProjectSectionTwo />
-        <ProjectSection />
         <AboutSection />
         <Links />
       </>
