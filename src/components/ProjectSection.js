78186f74813b4644f93a84051fc4ff880dcd1b4a
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-16 13:30:26 +0100

    Added project section

---
+++ src/components/ProjectSection.js
@@ -0,0 +1,68 @@
+import React from "react";
+import GithubIcon from "./IMG/github-icon.png";
+
+function ProjectSection(props) {
+  const linkItems = props.info
+    .map((item) => item.techText)
+    .toString()
+    .split(",");
+  const imageURL = props.info.map((item) => item.image);
+
+  return (
+    <>
+      <div className="p-container">
+        <img
+          className="p-image"
+          src={require(`./IMG/${imageURL}.png`)}
+          alt="screenshot"
+        ></img>
+        <div className="p-container-right">
+          {props.info.map((item) => (
+            <h1 className="p-title">{item.title}</h1>
+          ))}
+
+          {props.info.map((item) => (
+            <h2 className="p-subtitle">{item.subtitle}</h2>
+          ))}
+
+          {props.info.map((item) => (
+            <div className="p-para">{item.para}</div>
+          ))}
+
+          <div className="p-link-flexbox">
+            {" "}
+            {linkItems.map((item) => (
+              <li className="p-links">{item}</li>
+            ))}
+          </div>
+
+          <div className="p-icons">
+            <a
+              href={props.info.map((item) => item.githubLink)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="p-github-link"
+                src={GithubIcon}
+                alt="github"
+              ></img>
+            </a>
+
+            <div className="p-view-link">
+              <a
+                href={props.info.map((item) => item.viewLink)}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                VIEW PROJECT
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProjectSection;
