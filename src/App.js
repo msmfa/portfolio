c6530eaacabd5ecd9a7d631115936f7c723bef96
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-16 11:26:43 +0100

    Pushed data into it's own module

--- src/App.js
+++ src/App.js
@@ -13,48 +13,7 @@ import Links from "./components/Links";
 import ProjectHeader from "./components/ProjectHeader";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
-
-const projectOne = [
-  {
-    title: "Covis",
-    subtitle: "Data Visualiser for Covid-19",
-    para: `This project was buit to help people visualise the impact of
-  corona virus. I wanted to solve the problem of large data sets not
-  being easy to understand. The App fetches real time data from John
-  Hopskins University through an API and displays human icons to
-  represent how many people are infected, cured and have died.`,
-    techText: ["React", "Mathroid API"],
-    viewLink: "https://msmfa.github.io/corona-dashboard/",
-    githubLink: "https://github.com/msmfa/corona-dashboard",
-    image: "corona-app-screenshot",
-  },
-];
-const projectTwo = [
-  {
-    title: "Javascript In 30 Words",
-    subtitle: "A pre-interview refresher for Javascript interviews",
-    para: `This project was built to solidify my understanding of Javascript
-    fundamentals and advanced topics. For a detailed breakdown on the
-    project you can read my article here.`,
-    techText: ["Javascript", "Jest"],
-    viewLink: "https://javascript-in-30-words.netlify.com/",
-    githubLink: "https://github.com/msmfa/javascript-in-30",
-    image: "j30screenshot",
-  },
-];
-const projectThree = [
-  {
-    title: "Sort",
-    subtitle: "Data Visualiser for Sorting Algorithms",
-    para: `This project was built to help visualise how different sorting
-    algorithms actually work. The sorting algorithms chosen were quick
-    sort, merge sort and insertion sort.`,
-    techText: ["Javascript", "React"],
-    viewLink: "https://sort-vis.netlify.com//",
-    githubLink: "https://github.com/msmfa/sorting-visualiser",
-    image: "sorting-visualiser",
-  },
-];
+import { projectOne, projectTwo, projectThree } from "./projectData";
 
 function App() {
   return (
@@ -66,7 +25,6 @@ function App() {
       <ProjectSection info={projectOne} />
       <ProjectSection info={projectTwo} />
       <ProjectSection info={projectThree} />
-
       <AboutSection />
       <Links />
     </>
