db7069345e94c44ae8fce8ed5a476c870e83dac6
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-18 16:39:16 +0100

    Updated componenents

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -29,49 +29,52 @@ function ProjectSection(props) {
             <div className="p-para">{item.para}</div>
           ))}
 
-          <div className="p-link-flexbox">
-            {" "}
-            {linkItems.map((item) => (
-              <li className="p-links">{item}</li>
-            ))}
-          </div>
-          <div className="more-info">
-            {" "}
-            <a
-              href={props.info.map((item) => item.moreInfo)}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read more
-            </a>
-          </div>
-
-          <div className="p-icons">
-            <a
-              href={props.info.map((item) => item.githubLink)}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="p-github-link"
-                src={GithubIcon}
-                alt="github"
-              ></img>
-            </a>
-            <div className="p-view-link">
-              <a
-                href={props.info.map((item) => item.viewLink)}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                VIEW PROJECT
-              </a>
-            </div>
-          </div>
+          <ProjectLinks linkItems={linkItems} info={props.info} />
         </div>
       </div>
     </>
   );
 }
 
+function ProjectLinks(props) {
+  return (
+    <>
+      <div className="p-link-flexbox">
+        {" "}
+        {props.linkItems.map((item) => (
+          <li className="p-links">{item}</li>
+        ))}
+      </div>
+      <div className="more-info">
+        {" "}
+        <a
+          href={props.info.map((item) => item.moreInfo)}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more
+        </a>
+      </div>
+
+      <div className="p-icons">
+        <a
+          href={props.info.map((item) => item.githubLink)}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="p-github-link" src={GithubIcon} alt="github"></img>
+        </a>
+        <div className="p-view-link">
+          <a
+            href={props.info.map((item) => item.viewLink)}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            VIEW PROJECT
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
 export default ProjectSection;
