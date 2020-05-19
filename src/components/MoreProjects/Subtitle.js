bb707a52f44351224a24b9a28b592fdbb17c4211
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-05-19 13:51:41 +0100

    Added link to NPM package in github repo section

--- src/components/MoreProjects/Subtitle.js
+++ src/components/MoreProjects/Subtitle.js
@@ -5,6 +5,13 @@ export function Subtitle() {
     <h2 className="more-subtitle">
       This is an automatically updated feed of my latest projects built using
       the Github API.{" "}
+      <a
+        href="https://www.npmjs.com/package/github-repo-display-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Available as a NPM Package for React.
+      </a>{" "}
     </h2>
   );
 }
